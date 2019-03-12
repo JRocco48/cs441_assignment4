021e77c6049a74eef1ec0c219fc523294af4b363
John Rocco <jrocco2@bl-n11.pods.bu.int>
2019-03-12 16:07:37 -0400

    Began implementation of connecting labels to code

--- Sudoku Solver/Sudoku Solver/ViewController.m
+++ Sudoku Solver/Sudoku Solver/ViewController.m
@@ -9,6 +9,65 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *cell0;
+@property (weak, nonatomic) IBOutlet UITextField *cell1;
+@property (weak, nonatomic) IBOutlet UITextField *cell2;
+@property (weak, nonatomic) IBOutlet UITextField *cell3;
+@property (weak, nonatomic) IBOutlet UITextField *cell4;
+@property (weak, nonatomic) IBOutlet UITextField *cell5;
+@property (weak, nonatomic) IBOutlet UITextField *cell6;
+@property (weak, nonatomic) IBOutlet UITextField *cell7;
+@property (weak, nonatomic) IBOutlet UITextField *cell8;
+
+@property (weak, nonatomic) IBOutlet UITextField *cell9;
+@property (weak, nonatomic) IBOutlet UITextField *cell10;
+@property (weak, nonatomic) IBOutlet UITextField *cell11;
+@property (weak, nonatomic) IBOutlet UITextField *cell12;
+@property (weak, nonatomic) IBOutlet UITextField *cell13;
+@property (weak, nonatomic) IBOutlet UITextField *cell14;
+@property (weak, nonatomic) IBOutlet UITextField *cell15;
+@property (weak, nonatomic) IBOutlet UITextField *cell16;
+@property (weak, nonatomic) IBOutlet UITextField *cell17;
+
+@property (weak, nonatomic) IBOutlet UITextField *cell18;
+@property (weak, nonatomic) IBOutlet UITextField *cell19;
+@property (weak, nonatomic) IBOutlet UITextField *cell20;
+@property (weak, nonatomic) IBOutlet UITextField *cell21;
+@property (weak, nonatomic) IBOutlet UITextField *cell22;
+@property (weak, nonatomic) IBOutlet UITextField *cell23;
+@property (weak, nonatomic) IBOutlet UITextField *cell24;
+@property (weak, nonatomic) IBOutlet UITextField *cell25;
+@property (weak, nonatomic) IBOutlet UITextField *cell26;
+
+@property (weak, nonatomic) IBOutlet UITextField *cell27;
+@property (weak, nonatomic) IBOutlet UITextField *cell28;
+@property (weak, nonatomic) IBOutlet UITextField *cell29;
+@property (weak, nonatomic) IBOutlet UITextField *cell30;
+@property (weak, nonatomic) IBOutlet UITextField *cell31;
+@property (weak, nonatomic) IBOutlet UITextField *cell32;
+@property (weak, nonatomic) IBOutlet UITextField *cell33;
+@property (weak, nonatomic) IBOutlet UITextField *cell34;
+@property (weak, nonatomic) IBOutlet UITextField *cell35;
+
+@property (weak, nonatomic) IBOutlet UITextField *cell36;
+@property (weak, nonatomic) IBOutlet UITextField *cell37;
+@property (weak, nonatomic) IBOutlet UITextField *cell38;
+@property (weak, nonatomic) IBOutlet UITextField *cell39;
+@property (weak, nonatomic) IBOutlet UITextField *cell40;
+@property (weak, nonatomic) IBOutlet UITextField *cell41;
+@property (weak, nonatomic) IBOutlet UITextField *cell42;
+@property (weak, nonatomic) IBOutlet UITextField *cell43;
+@property (weak, nonatomic) IBOutlet UITextField *cell44;
+
+@property (weak, nonatomic) IBOutlet UITextField *cell45;
+@property (weak, nonatomic) IBOutlet UITextField *cell46;
+@property (weak, nonatomic) IBOutlet UITextField *cell47;
+@property (weak, nonatomic) IBOutlet UITextField *cell48;
+@property (weak, nonatomic) IBOutlet UITextField *cell49;
+@property (weak, nonatomic) IBOutlet UITextField *cell50;
+@property (weak, nonatomic) IBOutlet UITextField *cell51;
+@property (weak, nonatomic) IBOutlet UITextField *cell52;
+@property (weak, nonatomic) IBOutlet UITextField *cell53;
 
 @end
 
@@ -16,7 +75,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+     _cell0.text = @"0";
 }
 
 
